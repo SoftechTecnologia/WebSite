cd147f5b39bb0ac125c9c015c1db8b4454a31b9d
Softech <softechtecnologia@gmail.com>
2014-07-21 23:22:45 -0300

    Flexslider
    Adicionado Flexslider e realizadas alterações minoritárias.

--- js/js.js
+++ js/js.js
@@ -1,10 +1,11 @@
-function topo(){
-	prompt('O site está em fase de desenvolvimento. Caso queira entrar em contato para consulta dos serviços, favor, envie um e-mail. Desculpe o transtorno. Atenciosamente, Softech.','softechtecnologia@gmail.com'); 
-	$.ajax({
-		url:'index.html',
-		success:function(copia){
-			copia=$(copia).find('div#topo,');
-			$('#copy-topo').html(copia);
+$(function(){
+	SyntaxHighlighter.all();
+});
+$(window).load(function(){
+	$('.flexslider').flexslider({
+		animation: "slide",
+		start: function(slider){
+			$('body').removeClass('loading');
 		}
 	});
-}
+});
