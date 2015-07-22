068e06c9ffbbd309b3918ff5ec9d8b9406d3cadb
Thiago Jourdan <thiagojourdan@gmail.com>
2015-07-21 23:25:21 -0300

    Correção de bug de email
    Corrigido bug que impedia envio de email: mudança prévia no nome do
    botão "submit" sem atualização no ".php" de envio do email.

--- m/envio-de-email/sendmail.php
+++ m/envio-de-email/sendmail.php
@@ -1,18 +1,12 @@
 <?php
-$accesstype="Mobile";
-$myemail="adm@softechtecnologia.hol.es";
+$myemail="softechtecnologia@gmail.com";
 $name=$_POST['name'];
 $senderemail=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
-$emailmessage="Este é um contato realizado através da versão $accesstype do site \"www.softechtecnologia.hol.es\", por $name, com as seguintes informações:\n\nNome: $name\nE-mail: $senderemail\nAssunto: $subject\n\nMensagem: $message";
-if($_POST['button-sender']){
-	if(mail("$myemail,$senderemail","$subject - enviado pelo site","$emailmessage")){
-		header("Location:/m/envio-de-email/enviado-com-sucesso.html");
-	}
-	else{
-		header("Location:/m/envio-de-email/erro-ao-enviar.html");
-	}
+$emailmessage="Este é um contato realizado através da versão \"Mobile\" do site \"http://www.softechtecnologia.hol.es/m/\", por $name, com as seguintes informações:\n\nNome: $name\nE-mail: $senderemail\nAssunto: $subject\n\nMensagem: $message";
+if($_POST['buttonSender']||mail("$myemail,$senderemail","$subject - Enviado do site da Softech","$emailmessage")){
+	header("Location:/m/envio-de-email/enviado-com-sucesso.html");
 }
 else{
 	header("Location:/m/envio-de-email/erro-ao-enviar.html");
